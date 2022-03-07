1f273927a875b4a2a82d4779c0d7b37c5fa4b3d6
Çağatay Türkan <cagatayturkann@gmail.com>
2022-03-07 04:06:25 +0300

    file system updated, some uı changes made

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const fn = function () {
-  console.log('function works');
-};
+const express = require('express');
+const ejs = require('ejs')
+const path = require('path');
 
-fn();
+const app = express();
+
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs') //template engine views klasörü içerisine bakar
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTE
+app.get('/', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'views/index.html')); //middleware ile index dosyasını çağırdık. 
+  res.render('index')
+});
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+app.get('/add', (req, res) => {
+  res.render('add')
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`);
+});
