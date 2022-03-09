a39095745b659746b60f9903896ea1eb3befc8de
Çağatay Türkan <cagatayturkann@gmail.com>
2022-03-09 04:23:01 +0300

    image upload developed

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 const Photo = require('./models/Photo');
 
 const app = express();
@@ -23,11 +25,12 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(fileUpload());
 
 //ROUTE
 app.get('/', async (req, res) => {
   // res.sendFile(path.resolve(__dirname, 'views/index.html')); //middleware ile index dosyasını çağırdık.
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort('-dateCreated');
   res.render('index', {
     photos,
   });
@@ -51,8 +54,26 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
+  const uploadDir = 'public/uploads';
+  if (!fs.existsSync(uploadDir)) {
+    //klasör var mı yok mu bak. yoksa oluştur.
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadedImage = req.files.image; //image'ı yakaladık.
+  let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name; //dosya yolunu belirttik.
+
+  uploadedImage.mv(uploadPath, async () => {
+    //taşıma fonksiyonu. ilk parametre yol, ikinci parametre ekleme fonksiyonu
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name,
+    });
+    res.redirect('/');
+  });
+
+  // await Photo.create(req.body);
+  // res.redirect('/');
 });
 
 const port = 3000;
